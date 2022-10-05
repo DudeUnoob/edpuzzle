7e7782b6a4848575675adbd994150ccedf7127d7
DudeUnoob <to.baladev@gmail.com>
2022-10-04 22:49:26 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const bodyParser = require("body-parser")
+const app = express()
+const axios = require('axios')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+
+app.use(session({
+    secret: "helloworld",
+    saveUninitialized:true,
+    
+    resave: false 
+}));
+app.use(bodyParser.json())
+app.use(express.json());
+app.use(cookieParser());
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+let edpuzzleData
+app.post('/edpuzzle/token', (req, res) => {
+    const token = req.body.token
+
+    // axios.get('https://edpuzzle.com/api/v3/classrooms/active',{
+    //     headers:{"Authorization":`Bearer ${token}`}
+    // }).then(data => console.log(data))
+
+    fetch('https://edpuzzle.com/api/v3/classrooms/active',{
+        headers:{
+            "Authorization":`Bearer ${token}`
+        }
+    }).then(res => res.json()).then(data => {
+        if(data.error){
+            return res.status(400).send("invalid token")
+        } else {
+            edpuzzleData = data;
+            req.session.token = token;
+            req.session.valid = true;
+            req.session.edpuzzleData = data;
+
+            return res.redirect('/dashboard')
+        }
+    })
+
+    
+    
+})
+
+app.get('/dashboard', (req, res) => {
+    
+    //console.log(edpuzzleData)
+    res.render('dashboard', { edpuzzleData: req.session.edpuzzleData, token: req.session.token,  })
+})
+
+app.listen(3000, () => {
+    console.log('http://localhost:3000')
+})
