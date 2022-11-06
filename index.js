acd5651ccbc283ad8b86655fe85f348d2419091c
DudeUnoob <to.baladev@gmail.com>
2022-11-06 09:24:55 -0600

    finally pushed frontend and kahoot dev

--- index.js
+++ index.js
@@ -5,13 +5,17 @@ const axios = require('axios')
 const cookieParser = require('cookie-parser')
 const session = require('express-session')
 const path = require('path')
-const fetch = require('cross-fetch')
+const { response } = require('express')
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+const cors = require('cors')
 app.use(session({
     secret: "helloworld",
-    saveUninitialized:true,
-    
-    resave: false 
+    saveUninitialized: true,
+
+    resave: false
 }));
+
+app.use(cors())
 app.use(bodyParser.json())
 app.use(express.json());
 app.use(cookieParser());
@@ -20,10 +24,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
 
-   
+
     res.render('index')
 
-    
+
 })
 
 let edpuzzleData
@@ -34,12 +38,12 @@ app.post('/edpuzzle/token', (req, res) => {
     //     headers:{"Authorization":`Bearer ${token}`}
     // }).then(data => console.log(data))
 
-    fetch('https://edpuzzle.com/api/v3/classrooms/active',{
-        headers:{
-            "Authorization":`Bearer ${token}`
+    fetch('https://edpuzzle.com/api/v3/classrooms/active', {
+        headers: {
+            "Authorization": `Bearer ${token}`
         }
     }).then(res => res.json()).then(data => {
-        if(data.error){
+        if (data.error) {
             return res.status(400).send("invalid token")
         } else {
             edpuzzleData = data;
@@ -51,20 +55,20 @@ app.post('/edpuzzle/token', (req, res) => {
         }
     })
 
-    
-    
+
+
 })
 
 app.get('/dashboard', (req, res) => {
-    
-    if(!req.session.token){
+
+    if (!req.session.token) {
         res.status(400).send("Please <a href=/>login</a> with your edpuzzle token")
     }
     else {
-        res.render('dashboard', { edpuzzleData: req.session.edpuzzleData, token: req.session.token,  })
+        res.render('dashboard', { edpuzzleData: req.session.edpuzzleData, token: req.session.token, })
     }
-   // console.log(edpuzzleData)
-    
+    // console.log(edpuzzleData)
+
 })
 
 app.get('/logout', (req, res) => {
@@ -75,73 +79,73 @@ app.get('/logout', (req, res) => {
 
 let id;
 app.get('/edpuzzle/classroom/:id', (req, res) => {
-    id=req.params.id
-    fetch(`https://edpuzzle.com/api/v3/assignments/classrooms/${req.params.id}/students?needle=`,{
-        headers:{
-            "Authorization":`Bearer ${req.session.token}`
+    id = req.params.id
+    fetch(`https://edpuzzle.com/api/v3/assignments/classrooms/${req.params.id}/students?needle=`, {
+        headers: {
+            "Authorization": `Bearer ${req.session.token}`
         }
     })
-    .then(res => res.json()).then(data => {
-        console.log(data)
-        return res.render('room', { id: req.params.id, token: req.session.token, data: data})
+        .then(res => res.json()).then(data => {
+            console.log(data)
+            return res.render('room', { id: req.params.id, token: req.session.token, data: data })
 
-    })
+        })
 
 })
 
-app.get('/test',(req, res) => {
-    fetch(`https://edpuzzle.com/api/v3/assignments/classrooms/${id}/students?needle=`,{
-        headers:{
-            "Authorization":`Bearer ${req.session.token}`
+app.get('/test', (req, res) => {
+    fetch(`https://edpuzzle.com/api/v3/assignments/classrooms/${id}/students?needle=`, {
+        headers: {
+            "Authorization": `Bearer ${req.session.token}`
         }
     }).then(
         res => res.json()
-       ).then(data => res.send(data))
+    ).then(data => res.send(data))
 })
 
 app.get('/edpuzzle/room/:classroom_id/:lesson_id', (req, res) => {
     req.session.lesson_id = req.params.lesson_id;
     fetch(`https://edpuzzle.com/api/v3/assignments/classrooms/${req.params.classroom_id}/students?needle=`, {
-        headers:{
-            "Authorization":`Bearer ${req.session.token}`
+        headers: {
+            "Authorization": `Bearer ${req.session.token}`
         }
     }).then(
         res => res.json()
-    ).then(data => res.render('lesson', { classroom_id: req.params.classroom_id, token: req.session.token, data: data, lesson_id: req.params.lesson_id}))
+    ).then(data => res.render('lesson', { classroom_id: req.params.classroom_id, token: req.session.token, data: data, lesson_id: req.params.lesson_id }))
 })
 
 app.get('/test2', (req, res) => {
-    fetch(`https://edpuzzle.com/api/v3/assignments/classrooms/${id}/students?needle=`,{
-        headers:{
-            "Authorization":`Bearer ${req.session.token}`
+    fetch(`https://edpuzzle.com/api/v3/assignments/classrooms/${id}/students?needle=`, {
+        headers: {
+            "Authorization": `Bearer ${req.session.token}`
         }
     }).then(
         res => res.json()
-       ).then(data => res.send(data))
+    ).then(data => res.send(data))
 })
 
-app.get('/lesson_id',(req, res) => {
-    res.send({ lesson_id: req.session.lesson_id})
+app.get('/lesson_id', (req, res) => {
+    res.send({ lesson_id: req.session.lesson_id })
 })
 
-app.get('/edpuzzlehax1.mp4',(req, res) => {
-    res.sendFile('edpuzzlehax1.mp4',{ root: path.join(__dirname,'./public')})
+app.get('/edpuzzlehax1.mp4', (req, res) => {
+    res.sendFile('edpuzzlehax1.mp4', { root: path.join(__dirname, './public') })
 })
 
 app.get('/apple-touch-icon.png', (req, res) => {
-    res.sendFile('apple-touch-icon.png', { root: path.join(__dirname, './public/favicons')})
+    res.sendFile('apple-touch-icon.png', { root: path.join(__dirname, './public/favicons') })
 })
 
 app.get('/favicon-32x32.png', (req, res) => {
-    res.sendFile('favicon-32x32.png', { root: path.join(__dirname, './public/favicons')})
+    res.sendFile('favicon-32x32.png', { root: path.join(__dirname, './public/favicons') })
 })
 
 app.get('/favicon-16x16.png', (req, res) => {
-    res.sendFile('favicon-16x16.png', { root: path.join(__dirname, './public/favicons')})
+    res.sendFile('favicon-16x16.png', { root: path.join(__dirname, './public/favicons') })
 })
 
 app.get('/site.webmanifest', (req, res) => {
-    res.sendFile('site.webmanifest', { root: path.join(__dirname, './public/favicons')})
+    res.sendFile('site.webmanifest', { root: path.join(__dirname, './public/favicons') })
 })
 
 
@@ -151,20 +155,26 @@ app.post('/edpuzzle/login', (req, res) => {
 
     console.log(username, password)
 
-    axios.post('https://edpuzzle.com/api/v3/users/login', {
-        username: username,
-        password: password,
-        role:"student"
-    }).then(function (response) {
-        req.session.token = response.headers.authorization.slice(7);
+    fetch('http://localhost:443')
+        .then(ok => ok.json())
+        .then(get => {
 
-        return res.redirect('/dashboard')
-    })
-    .catch(function (error){
-        return res.status(400).send(error)
-    })
+            fetch('http://localhost:443/login', {
+                method:"post",
+                body:JSON.stringify({
+                    username: username,
+                    password: password,
+                    role:"student"
+                })
+            })
+        })
 
-   
+
+
+})
+
+app.get('/edpuzzle/info', (req, res) => {
+    res.render('edpuzzleInfo')
 })
 
 
