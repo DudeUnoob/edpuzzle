47af312135533cc2c23fc92ef5b3d315d31c3d2d
DudeUnoob <to.baladev@gmail.com>
2022-11-08 22:18:22 -0600

    production auth route

--- server/auth/discord.js
+++ server/auth/discord.js
@@ -1,6 +1,7 @@
 //https://discord.com/api/oauth2/authorize?client_id=1039205411934453831&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Frouter%2Fapi%2Fv1%2Fdiscord&response_type=code&scope=identify%20email%20guilds%20connections
 const passport = require('passport')
 const { Strategy } = require('passport-discord')
+const host = "https://puzzlehax.ml"
 
 
 // clientID:"1039205411934453831",
@@ -26,7 +27,7 @@ passport.use(
     new Strategy({
         clientID:"1039205411934453831",
         clientSecret:"ADstjN5W1xReD-5pAgma42BbA-cgFVj4",
-        callbackURL:"http://localhost:3000/router/api/auth/redirect",
+        callbackURL:`${host}/router/api/auth/redirect`,
         scope:['identify','email','guilds','guilds.join']
         
         
