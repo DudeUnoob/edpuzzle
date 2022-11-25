5059395562162b5dc8c6110fbc5b12fa931296da
DudeUnoob <to.baladev@gmail.com>
2022-11-24 20:00:25 -0600

    new kahoot botter

--- server/api.js
+++ server/api.js
@@ -1,10 +1,15 @@
 
 const express = require('express')
 const apiRouter = express.Router();
+const bodyParser = require('body-parser')
 const path = require('path')
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 const config = require("./config/botconfig.json")
 
+
+apiRouter.use(bodyParser.urlencoded({ extended: true }));
+apiRouter.use(express.json())
+apiRouter.use(bodyParser.json())
 apiRouter.get('/v1/user', (req, res) => {
 
     res.send(req.session.passport)
@@ -61,4 +66,55 @@ apiRouter.get('/v1/guild/members', (req, res) => {
     }).then(response => response.json()).then(data => res.send(data))
 })
 
+
+apiRouter.post('/kahoot/bots', (req, res) => {
+    const gamePin = req.body.gamePin
+    const kahootBots = req.body.botAmount
+    const kahootBotName = req.body.botName
+    
+    
+    fetch(`https://discord.com/api/v10/guilds/1039724305795252295/members/${req.body.userId}`, {
+        headers:{
+            'Content-Type': 'application/json',
+            'Authorization': `Bot ${config.token}`
+        }
+    }).then(response => response.json())
+    .then(data => {
+        const filter = data.roles.filter(role => role == "1044668796771782716")
+
+        if(filter.length == 0){
+
+            
+            return res.send({ message: "Sorry you don't have premium, get it here at http://patreon.com/DomK" })
+
+        } else {
+            
+            fetch('https://kahootbotter.com/api/graphql', {
+                method:"post",
+                headers:{
+                    'Content-Type':"application/json"
+                },
+                body:JSON.stringify({
+                    query: "mutation spawnBots($botName: String!, $gamePin: Int!, $botAmount: Int!, $sessionId: String!) {\n  spawnBots(\n    botName: $botName\n    gamePin: $gamePin\n    botAmount: $botAmount\n    sessionId: $sessionId\n  ) {\n    title\n    status\n    description\n    __typename\n  }\n}\n",
+                    operationName: "spawnBots",
+                    variables: {
+                      botName: kahootBotName,
+                      gamePin: parseInt(gamePin),
+                      botAmount: parseInt(kahootBots),
+                      sessionId: "138.199.9.179"
+                    },
+                    
+                  })
+            }).then(get => get.json())
+            .then(data => {
+                return res.send({ message: data })
+            })
+        }
+        
+    })
+})
+
+apiRouter.get('/user', (req, res) => {
+    res.send({ user: req.session.passport.user.user })
+})
 module.exports = apiRouter;
